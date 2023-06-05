95857299f3f921b614756d218eeff463edaee682
RishiSetpal <rishisetpal123@gmail.com>
2023-06-05 15:18:42 +0530

    Update javascript1.js
    Add secured address API end point

--- javascript1.js
+++ javascript1.js
@@ -2,7 +2,7 @@ let generateMsg = () => {
     event.preventDefault();
     let message = document.getElementById("msg");
 
-    let urlAddress = "http://api.quotable.io/random";
+    let urlAddress = "https://api.quotable.io/random";
     
     fetch(urlAddress)
     .then((response) => response.json())
